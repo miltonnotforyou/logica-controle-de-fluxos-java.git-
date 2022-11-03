d3fa8bf34ab45e4c8228304133bbbac1327201bc
Miltonnnotforyou <67373586+miltonnotforyou@users.noreply.github.com>
2022-11-03 17:38:12 -0300

    Exercicio 2

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -12,8 +12,8 @@ public class Exercicio2 {
         System.out.println("b2 || b3 " + (b2 || b3));//-> || disjunção
         System.out.println("b2 || b4 " + (b2 || b4));//-> || disjunção
 
-        System.out.println("b1 ^ b3 " + (b1 ^ b2));//-> ^ disjunção exclusiva
-        System.out.println("b4 ^ b1 " + (b1 ^ b2));//-> ^ disjunção exclusiva
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));//-> ^ disjunção exclusiva
+        System.out.println("b4 ^ b1 " + (b4 ^ b1));//-> ^ disjunção exclusiva
 
         System.out.println(!b1);//-> ! negação
         System.out.println(!b2);//-> ! negação
@@ -25,7 +25,7 @@ public class Exercicio2 {
         System.out.println("((i1 + i2) < (f2 - f1)) && true " + ((i1 + i2 < (f2 - f1)) && true));
         System.out.println("(i1 > i2) || (f2 < f1)) " + ((i1 > i2) || (f2 < f1)));
 
-        double salarioMensal = 11893.58d;
+        double salarioMensal = 10893.58d;
         double mediaSalario = 10500d;
 
         int quantidadeDependentes = 4;
@@ -38,8 +38,8 @@ public class Exercicio2 {
 
         System.out.println((salarioBaixo) && (muitoaDependentes));
 
-        boolean recebeAuxilio = (salarioBaixo) && (muitoaDependentes);
-        System.out.println("Recebe Auxilio: " + recebeAuxilio);
+        boolean recebeAuxilio = (salarioBaixo) && (muitoaDependentes);//-> simplificado
+        System.out.println("Recebe Auxilio: " + recebeAuxilio);//-> simplificado
     }
 
 }
